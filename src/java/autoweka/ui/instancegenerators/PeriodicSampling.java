c950acdf7f819ea167029fad38960536a472c36b
Manuel Martín <draxus@gmail.com>
2015-09-24 17:09:45 +0100

    Add PeriodicSampling generator

---
+++ src/java/autoweka/ui/instancegenerators/PeriodicSampling.java
@@ -0,0 +1,40 @@
+package autoweka.ui.instancegenerators;
+
+import java.awt.Frame;
+import java.util.Properties;
+
+import javax.swing.JTextField;
+
+import org.javabuilders.swing.SwingJavaBuilder;
+
+import autoweka.ui.PropertyPanel;
+
+public class PeriodicSampling extends PropertyPanel
+{
+    private JTextField mNumFoldsText;
+    public PeriodicSampling(Frame owner)
+    {
+        super(owner);
+        SwingJavaBuilder.build(this);
+    }
+    
+    @Override
+    public String getClassName()
+    {
+        return "autoweka.instancegenerators.PeriodicSampling";
+    }
+
+    @Override
+    public Properties getProperties()
+    {
+        Properties props = new Properties();
+        props.setProperty("numFolds", mNumFoldsText.getText());
+        return props;
+    }
+
+    public String toString()
+    {
+        return "PeriodicSampling";
+    }
+}
+
