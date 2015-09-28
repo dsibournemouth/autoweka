f7d96918c5411ef60dc73608c20b7357ba4fbf49
DraXus <draxus@gmail.com>
2015-09-28 20:39:40 +0100

    Reorganize imports of DensityPreservingSampling.java

--- src/java/autoweka/instancegenerators/DensityPreservingSampling.java
+++ src/java/autoweka/instancegenerators/DensityPreservingSampling.java
@@ -1,27 +1,23 @@
 
 package autoweka.instancegenerators;
 
-import autoweka.InstanceGenerator;
-import autoweka.Util;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.Random;
 
 import weka.core.DenseInstance;
 import weka.core.Instances;
-import weka.core.Utils;
 import weka.core.converters.ArffSaver;
+import autoweka.InstanceGenerator;
+import autoweka.Util;
 
-import com.mathworks.toolbox.javabuilder.*;
+import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
 
-import dps.*;
+import dps.DPS;
 
 // In order to work, PATH has to include C:\Program Files\MATLAB\MATLAB Runtime\v85\runtime\win64\
 /**
