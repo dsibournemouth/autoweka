7bab9680e9d5bebdebbe04dc544d10d9abbe3fb7
DraXus <draxus@gmail.com>
2015-10-24 18:38:11 +0100

    Cluster by CV error

--- scripts/get_configurations.py
+++ scripts/get_configurations.py
@@ -6,7 +6,7 @@ from table_configurations import parse_configuration, get_results
 from plot_dendograms import plot_dendogram
 
 
-def create_distance_matrix(configurations):
+def create_distance_matrix_by_configuration(configurations):
     rows = len(configurations)
     if rows == 0:
         return 0
@@ -29,16 +29,30 @@ def create_distance_matrix(configurations):
 
     return distance
 
+def create_distance_matrix_by_error(errors):
+    rows = len(errors)
+    if rows == 0:
+        return 0
 
-def sub_main(dataset, strategy, generation):
+    distance = np.zeros((rows, rows))
+    # Each row is an error value
+    for i in range(0, rows):
+        for j in range(0, rows):
+            # distance == 0 if they have the same error
+            distance[i, j] = abs(errors[i] - errors[j])
+
+    return distance
+
+def sub_main(dataset, strategy, generation, plot_flags):
     results, best_error_seed, best_test_error_seed = get_results(dataset, strategy, generation)
     configurations = []
+    errors = []
     labels = []
     for result in results:
         seed = result[3]
         params = parse_configuration(result[4], True)
-        error = result[5]
-        configurations.append([
+        error = float(result[5])
+        flow = [
             params['missing_values']['method'],
             params['outliers']['method'],
             params['transformation']['method'],
@@ -46,13 +60,23 @@ def sub_main(dataset, strategy, generation):
             params['sampling']['method'],
             params['predictor']['method'],
             params['meta']['method']
-        ])
-        labels.append("%s (%.2f)" % (seed, error))
+        ]
+        configurations.append(flow)
+        errors.append(error)
+        flow_string = ""
+        for i in reversed(range(0, len(flow))):
+            flow_string += flow[i].split(".")[-1] + " "
+        labels.append("#%s (%.2f) %s" % (seed, error, flow_string))
 
-    matrix = create_distance_matrix(configurations)
-    np.savetxt("../distances/%s.%s.%s.csv" % (dataset, strategy, generation), matrix, fmt="%.6f", delimiter=",")
-
-    plot_dendogram(matrix, labels, "%s.%s.%s" % (dataset, strategy, generation))
+    if plot_flags['by_configuration']:
+        matrix = create_distance_matrix_by_configuration(configurations)
+        np.savetxt("../distances/by_configuration/%s.%s.%s.csv" % (dataset, strategy, generation), matrix, fmt="%.6f", delimiter=",")
+        plot_dendogram(matrix, labels, "%s.%s.%s" % (dataset, strategy, generation), "../distances/by_configuration")
+    
+    if plot_flags['by_error']:
+        matrix = create_distance_matrix_by_error(errors)
+        np.savetxt("../distances/by_error/%s.%s.%s.csv" % (dataset, strategy, generation), matrix, fmt="%.6f", delimiter=",")
+        plot_dendogram(matrix, labels, "%s.%s.%s" % (dataset, strategy, generation), "../distances/by_error")
 
 
 def main():
@@ -60,19 +84,24 @@ def main():
     parser.add_argument('--dataset', choices=datasets, required=False)
     parser.add_argument('--strategy', choices=strategies, required=False)
     parser.add_argument('--generation', choices=generations, required=False)
-
+    parser.add_argument('--plot-by-configuration', action="store_true")
+    parser.add_argument('--plot-by-error', action="store_true")
+    
     args = parser.parse_args()
 
     # override default values
     selected_datasets = [args.dataset] if args.dataset else datasets
     selected_strategies = [args.strategy] if args.strategy else strategies
     selected_generations = [args.generation] if args.generation else generations
+    
+    plot_flags = {'by_configuration': args.plot_by_configuration,
+                  'by_error': args.plot_by_error}
 
     for dataset in selected_datasets:
         for strategy in selected_strategies:
             for generation in selected_generations:
                 try:
-                    sub_main(dataset, strategy, generation)
+                    sub_main(dataset, strategy, generation, plot_flags)
                 except Exception as e:
                     print e
                     continue
--- scripts/plot_dendograms.py
+++ scripts/plot_dendograms.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 methods = ['single', 'complete', 'average', 'centroid', 'median', 'ward']
 
-def plot_dendogram(distance_matrix, labels, title):
+def plot_dendogram(distance_matrix, labels, title, folder):
     for cluster_method in methods:
         cluster = linkage(distance_matrix, method=cluster_method)
-        fig, ax = plt.subplots(figsize=(10, 15))  # set size
+        fig, ax = plt.subplots(figsize=(25, 15))  # set size
         plt.title(title)
         ax = dendrogram(cluster, labels=labels, orientation='right')
 
         plt.tight_layout()
 
-        plt.savefig('../distances/%s.%s.png' % (title, cluster_method), dpi=200)
+        plt.savefig('%s/%s.%s.png' % (folder, title, cluster_method), dpi=200)
         plt.close("all")
