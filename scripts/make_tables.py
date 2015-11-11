b12bc486edfe6d8b66cd217636d8672388e77256
Manuel Martín <draxus@gmail.com>
2015-11-11 23:20:47 +0000

    Remove make_tables script

--- scripts/make_tables.py
+++
@@ -1,8 +0,0 @@
-from os import system
-from config import *
-
-system("python table_index.py")
-system("python table_configurations.py")
-for d in datasets:
-    system("python table_strategies.py --dataset=%s" % d)          
-    
