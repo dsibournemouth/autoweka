3e6acf39146f0645e980af308382fdaa26ff0ea0
Manuel Martín <draxus@gmail.com>
2016-04-26 22:37:14 +0100

    Refactoring

--- scripts/generate_batch_files.sh
+++ scripts/generate_batch_files.sh
@@ -1,8 +1,14 @@
 # Generate batches
 for batch in `seq 1 10`; do $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.filters.unsupervised.instance.RemoveFolds -i test.arff -o test$batch.arff -c last -S -1 -N 10 -F $batch; done
 
+# Generate incremental batches
+mkdir incremental; rm incremental/*arff; cp train.arff incremental/train0.arff; for batch in `seq 1 10`; do let prev=batch-1; $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.core.Instances append incremental/train$prev.arff test$batch.arff > incremental/train$batch.arff; done
+
 # Copy files from original run
 cp -R ../EXPERIMENTS_FOLDER/DATASET.SMAC.CV-DATASET .
+
+# Copy files from original run for incremental
+EXPERIMENT=$(echo "${PWD##*/}"); cp ../../experiments-adaptive/$EXPERIMENT/* . ; cp -R ../../experiments-adaptive/$EXPERIMENT/batch0 . ; mkdir out ; cp -R ../../experiments-adaptive/$EXPERIMENT/out/autoweka out/
 
 # Create scenario.1
 cp autoweka.scenario autoweka.scenario.1
@@ -15,6 +21,10 @@ vim autoweka.scenario.1
 
 # Duplicate scenarios
 for i in `seq 1 8`; do let next=i+1; let next2=i+2; sed "s/test2\./test${next2}\./g" autoweka.scenario.1 | sed "s/test1\./test${next}\./g" > autoweka.scenario.$((i+1)); done
+
+
+# Duplicate incremental scenarios
+for i in `seq 1 8`; do let next=i+1; let next2=i+2; sed "s/test2\./test${next2}\./g" autoweka.scenario.1 | sed "s/train1\./train${next}\./g" > autoweka.scenario.$((i+1)); done
 
 # Create experiment.1
 EXPERIMENT=$(echo "${PWD##*/}"); cp $EXPERIMENT.experiment $EXPERIMENT.experiment.1
@@ -29,8 +39,17 @@ vim $EXPERIMENT.experiment.1
 # Duplicate experiments
 EXPERIMENT=$(echo "${PWD##*/}"); for i in `seq 1 8`; do let next=i+1; let next2=i+2; sed "s/test2\./test${next2}\./g" $EXPERIMENT.experiment.1 | sed "s/test1\./test${next}\./g" | sed "s/scenario\.1/scenario\.${next}/g" | sed "s/batch1/batch${next}/g" | sed "s/adaptive1/adaptive${next}/g" | sed "s/autoweka\/state/adaptive${i}\/state/g" > $EXPERIMENT.experiment.$((i+1)); done
 
+
+# Duplicate incremental experiments
+EXPERIMENT=$(echo "${PWD##*/}"); for i in `seq 1 8`; do let next=i+1; let next2=i+2; sed "s/test2\./test${next2}\./g" $EXPERIMENT.experiment.1 | sed "s/train1\./train${next}\./g" | sed "s/scenario\.1/scenario\.${next}/g" | sed "s/batch1/batch${next}/g" | sed "s/adaptive1/adaptive${next}/g" | sed "s/autoweka\/state/adaptive${i}\/state/g" > $EXPERIMENT.experiment.$((i+1)); done
+
+
+
 # Run experiments
 python launch_experiments.py --config=CONFIG_FILE --dataset=DATASET --strategy=SMAC --batches=10
+
+# Move final batch to a new folder
+mkdir batch9 && mv *traj* batch9/ && mv predictions.* batch9/ && mv trained.* batch9/
 
 # Concatenate all predictions
 # TODO: extract predictions from batch0 (or maybe run the model on test1.arff?)
@@ -46,8 +65,9 @@ for seed in `seq 0 24`; do let total=`wc -l final/predictions.$seed.csv | awk {'
 
 
 
+for batch in `seq 0 9`; do mkdir batch$batch; done
 
-
+DATASET="IndustrialDrier"; for batch in `seq 0 9`; do scp msalvador@172.16.49.100:/home/msalvador/autoweka/experiments-incremental/$DATASET-3classes.SMAC.CV-$DATASET-3classes/batch$batch/predictions* batch$batch/; done
 
 # Manually parse SMAC trajectories and evaluate models in testing batch
 EXPERIMENT=$(echo "${PWD##*/}"); for seed in `seq 0 24`; do for batch in `seq 1 9`; do $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar autoweka.smac.SMACTrajectoryParser -single $PWD $seed -batchNumber $batch && $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar autoweka.TrajectoryPointPredictionRunner $PWD/$EXPERIMENT.trajectories.$seed -savemodel && mv predictions.$seed.csv batch$batch/ && mv trained.$seed.model batch$batch/ && mv $EXPERIMENT.trajectories.$seed batch$batch/; done; done
@@ -57,7 +77,12 @@ sed -i 's/test\.arff/test1\.arff/g' batch0/*trajectories*
 
 
 # Test first batch and save in batch0 folder (classification)
-DATASET_PATH="/home/msalvador/autoweka/datasets/catalyst_activation"; for seed in `seq 0 24`; do $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.classifiers.Evaluation weka.classifiers.misc.SerializedClassifier -T $DATASET_PATH/test1.arff -l batch0/trained.$seed.model -classifications "weka.classifiers.evaluation.output.prediction.CSV -p 0 -file batch0/predictions.$seed.csv"; done
+DATASET_PATH="/home/msalvador/autoweka/datasets/absorber"; rm batch0/predictions.*; for seed in `seq 0 24`; do $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.classifiers.Evaluation weka.classifiers.misc.SerializedClassifier -T $DATASET_PATH/test1.arff -l batch0/trained.$seed.model -classifications "weka.classifiers.evaluation.output.prediction.CSV -file batch0/predictions.$seed.csv" & done
 
-# Test first batch and save in batch0 folder (regression)
-DATASET_PATH="/home/msalvador/autoweka/datasets/catalyst_activation"; for seed in `seq 0 24`; do $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.classifiers.Evaluation weka.classifiers.misc.SerializedClassifier -T $DATASET_PATH/test1.arff -l batch0/trained.$seed.model -classifications "weka.classifiers.evaluation.output.prediction.CSV -file batch0/predictions.$seed.csv"; done
+DATASET_PATH="/home/msalvador/autoweka/datasets/ThermalOxidizerConv"; rm batch0/predictions.*; for seed in `seq 0 24`; do $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.classifiers.Evaluation weka.classifiers.misc.FreeSerializedClassifier -T $DATASET_PATH/test1.arff -t $DATASET_PATH/train.arff -l batch0/trained.$seed.model -classifications "weka.classifiers.evaluation.output.prediction.CSV -file batch0/predictions.$seed.csv" & done
+
+# Static
+DATASET_PATH="/home/msalvador/autoweka/datasets/ThermalOxidizerConv"; mkdir static; for seed in `seq 0 24`; do for batch in `seq 1 9`; do let next=batch+1; mkdir -p static/batch$batch; $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.classifiers.Evaluation weka.classifiers.misc.FreeSerializedClassifier -t $DATASET_PATH/test$batch.arff -T $DATASET_PATH/test$next.arff -l batch0/trained.$seed.model -classifications "weka.classifiers.evaluation.output.prediction.CSV -file static/batch$batch/predictions.$seed.csv"; done & done
+
+# Static Incremental
+DATASET_PATH="/home/msalvador/autoweka/datasets/debutanizer-3classes"; for seed in `seq 11 16`; do for batch in `seq 1 9`; do let next=batch+1; mkdir -p static/batch$batch; $MY_JAVA_PATH/java -cp $AUTOWEKA_PATH/autoweka.jar weka.classifiers.Evaluation weka.classifiers.misc.FreeSerializedClassifier -t $DATASET_PATH/incremental/train$batch.arff -T $DATASET_PATH/test$next.arff -l batch0/trained.$seed.model -classifications "weka.classifiers.evaluation.output.prediction.CSV -file static/batch$batch/predictions.$seed.csv"; done & done
--- scripts/parse_predictions_files.R
+++ scripts/parse_predictions_files.R
@@ -1,6 +1,8 @@
+library("RSQLite")
 library("matrixStats")
 library("ggplot2")
 library("reshape")
+library("Metrics")
 
 ###########################################
 # FROM DATABASE
@@ -21,110 +23,324 @@ results_min = dbGetQuery(con, sql_command)
 
 datasets = c("absorber", "debutanizer", "catalyst", "IndustrialDrier", "oxeno", "sulfur", "ThermalOxidizerConv")
 
-for (d in 1:length(datasets)) {
+run_classification = function(datasets){
   
-  print(datasets[d])
-  dataset = datasets[d]
-
-  test_error_adaptive = matrix(0, 25, 10)
-  for (i in 0:24) {
-  	for (j in 0:9) {
-  		filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\adaptive\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
-  		#print(paste("Reading", filename))
-  		if (file.exists(filename)) {
-  			predictions = read.csv(filename)
-  			batch = as.data.frame(predictions)
-  			test_error_adaptive[i+1, j+1] = 100*sum(batch$error == "+", na.rm=TRUE)/nrow(batch)
-  		}
-  		else {
-  			print(paste("File", filename, "does not exists"))
-  			test_error_adaptive[i+1,j+1] = NaN
-  		}
-  	}
+  for (d in 1:length(datasets)) {
+    
+    print(datasets[d])
+    dataset = datasets[d]
+    
+    all_instances = matrix(0, 5, 25)
+    all_test_error = matrix(0, 5, 25)
+    
+    test_error_adaptive = matrix(0, 25, 10)
+    for (i in 0:24) {
+      for (j in 0:9) {
+        filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\adaptive\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
+        #print(paste("Reading", filename))
+        if (file.exists(filename)) {
+          predictions = read.csv(filename)
+          batch = as.data.frame(predictions)
+          test_error_adaptive[i+1, j+1] = 100*sum(batch$error == "+", na.rm=TRUE)/nrow(batch)
+          all_instances[1,i+1] = all_instances[1,i+1] + nrow(batch)
+          all_test_error[1,i+1] = all_test_error[1,i+1] + sum(batch$error == "+", na.rm=TRUE)
+        }
+        else {
+          print(paste("File", filename, "does not exists"))
+          test_error_adaptive[i+1,j+1] = NaN
+          all_instances[1,i+1] = NaN
+          all_test_error[1,i+1] = NaN
+        }
+      }
+    }
+    
+    test_error_retrain = matrix(0, 25, 10)
+    for (i in 0:24) {
+      for (j in 0:9) {
+        filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\static\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
+        #print(paste("Reading", filename))
+        if (file.exists(filename)) {
+          predictions = read.csv(filename)
+          batch = as.data.frame(predictions)
+          test_error_retrain[i+1, j+1] = 100*sum(batch$error == "+", na.rm=TRUE)/nrow(batch)
+          all_instances[2,i+1] = all_instances[2,i+1] + nrow(batch)
+          all_test_error[2,i+1] = all_test_error[2,i+1] + sum(batch$error == "+", na.rm=TRUE)
+        }
+        else {
+          print(paste("File", filename, "does not exists"))
+          test_error_retrain[i+1,j+1] = NaN
+          all_instances[2,i+1] = NaN
+          all_test_error[2,i+1] = NaN
+        }
+      }
+    }
+    
+    test_error_static = matrix(0, 25, 10)
+    for (i in 0:24) {
+      filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\non-adaptive\\", dataset,"\\predictions.", i, ".csv", sep="")
+      if (file.exists(filename)) {
+        predictions = read.csv(filename)
+        batches  = split(predictions, ceiling(seq_along(predictions$inst.)/(nrow(predictions)/10)))
+        for (j in 1:10) {
+          batch = as.data.frame(batches[j])
+          test_error_static[i+1, j] = 100*sum(batch[4] == "+", na.rm=TRUE)/nrow(batch[4])
+          all_instances[3,i+1] = all_instances[3,i+1] + nrow(batch[4])
+          all_test_error[3,i+1] = all_test_error[3,i+1] + sum(batch[4] == "+", na.rm=TRUE)
+        }
+      }
+      else {
+        print(paste("File", filename, "does not exists"))
+        test_error_static[i+1,] = NaN
+        all_instances[3,i+1] = NaN
+        all_test_error[3,i+1] = NaN
+      }
+    }
+    
+    test_error_incremental_adaptive = matrix(0, 25, 10)
+    for (i in 0:24) {
+      for (j in 0:9) {
+        filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\incremental\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
+        #print(paste("Reading", filename))
+        if (file.exists(filename)) {
+          predictions = read.csv(filename)
+          batch = as.data.frame(predictions)
+          test_error_incremental_adaptive[i+1, j+1] = 100*sum(batch$error == "+", na.rm=TRUE)/nrow(batch)
+          all_instances[4,i+1] = all_instances[4,i+1] + nrow(batch)
+          all_test_error[4,i+1] = all_test_error[4,i+1] + sum(batch$error == "+", na.rm=TRUE)
+        }
+        else {
+          print(paste("File", filename, "does not exists"))
+          test_error_incremental_adaptive[i+1,j+1] = NaN
+          all_instances[4,i+1] = NaN
+          all_test_error[4,i+1] = NaN
+        }
+      }
+    }
+    
+    test_error_incremental_retrain = matrix(0, 25, 10)
+    for (i in 0:24) {
+      for (j in 0:9) {
+        filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\incremental-static\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
+        #print(paste("Reading", filename))
+        if (file.exists(filename)) {
+          predictions = read.csv(filename)
+          batch = as.data.frame(predictions)
+          test_error_incremental_retrain[i+1, j+1] = 100*sum(batch$error == "+", na.rm=TRUE)/nrow(batch)
+          all_instances[5,i+1] = all_instances[5,i+1] + nrow(batch)
+          all_test_error[5,i+1] = all_test_error[5,i+1] + sum(batch$error == "+", na.rm=TRUE)
+        }
+        else {
+          print(paste("File", filename, "does not exists"))
+          test_error_incremental_retrain[i+1,j+1] = NaN
+          all_instances[5,i+1] = NaN
+          all_test_error[5,i+1] = NaN
+        }
+      }
+    }
+    
+    m1 = as.data.frame(test_error_adaptive)
+    m1$id = "Batch + SMAC"
+    m2 = as.data.frame(test_error_retrain)
+    m2$id = "Batch"
+    m3 = as.data.frame(test_error_static)
+    m3$id = "Baseline"
+    m4 = as.data.frame(test_error_incremental_adaptive)
+    m4$id = "Incremental + SMAC"
+    m5 = as.data.frame(test_error_incremental_retrain)
+    m5$id = "Incremental"
+    df = rbind(m1, m2, m3, m4, m5)
+    
+    
+    g0 <- ggplot(data=melt(df, "id"), aes(as.factor(variable), value, colour=id, group=id))
+    
+    my.fun<-function(x){data.frame(ymin=min(x),ymax=max(x),y=mean(x))}
+    g_box = g0 + 
+      stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width=0.2, size=0.5, alpha=0.5) + 
+      stat_summary(fun.y = mean, geom = "line", aes(group=id, linetype=id), size=1.1) + 
+      scale_linetype_manual(values=c("solid", "dashed", "dotted", "dashed", "dotted")) +
+      theme_bw() + theme(panel.grid.major.x = element_line(colour = "gray"), panel.grid.major.y = element_blank(),
+                         panel.grid.minor.x = element_blank(), panel.grid.major.y = element_blank()) +
+      ylim(0,100) +
+      xlab("Batch") + ylab("% class. error") + ggtitle(dataset)
+    
+    g_box
+    ggsave(file=paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\plots-adaptive\\", dataset, ".png", sep=""))
+    
+    
+    names_error = c("Batch + SMAC", "Batch", "Baseline", "Incremental + SMAC", "Incremental")
+    all_text = ""
+    per_seed_text = ""
+    per_seed_error = data.frame(NA, 5, 25)
+    for(i in 1:5) {
+      current_error = matrix(NA, 25, 1)
+      for (j in 1:25) {
+        current_error[j] = all_test_error[i,j]/all_instances[i,j]
+        per_seed_error[i, j] = current_error[j]
+        
+      }
+      all_text = sprintf("%s\n%s, %f", all_text, names_error[i], mean(current_error, na.rm=TRUE))
+      per_seed_text = sprintf("%s\n%s, %f", per_seed_text, names_error[i], min(per_seed_error[i,], na.rm=TRUE))
+    }
+    write.csv(paste(all_text, per_seed_text, sep="\n"), file=paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\plots-adaptive\\", dataset, ".csv", sep=""))
   }
-  
-  test_error_retrain = matrix(0, 25, 10)
-  for (i in 0:24) {
-  	for (j in 0:9) {
-  		filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\static\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
-  		#print(paste("Reading", filename))
-  		if (file.exists(filename)) {
-  			predictions = read.csv(filename)
-  			batch = as.data.frame(predictions)
-  			test_error_retrain[i+1, j+1] = 100*sum(batch$error == "+", na.rm=TRUE)/nrow(batch)
-  		}
-  		else {
-  			print(paste("File", filename, "does not exists"))
-  			test_error_retrain[i+1,j+1] = NaN
-  		}
-  	}
-  }
-  
-  test_error_static = matrix(0, 25, 10)
-  for (i in 0:24) {
-  	filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\non-adaptive\\", dataset,"\\predictions.", i, ".csv", sep="")
-  	if (file.exists(filename)) {
-  		predictions = read.csv(filename)
-  		batches  = split(predictions, ceiling(seq_along(predictions$inst.)/(nrow(predictions)/10)))
-  		for (j in 1:10) {
-  			batch = as.data.frame(batches[j])
-  			test_error_static[i+1, j] = 100*sum(batch[4] == "+", na.rm=TRUE)/nrow(batch[4])
-  		}
-  	}
-  	else {
-  		print(paste("File", filename, "does not exists"))
-  		test_error_static[i+1,] = NaN
-  	}
-  }
-  
-  m1 = as.data.frame(test_error_adaptive)
-  m1$id = "Adapt"
-  m2 = as.data.frame(test_error_retrain)
-  m2$id = "Retrain"
-  m3 = as.data.frame(test_error_static)
-  m3$id = "Static"
-  df = rbind(m1, m2, m3)
-  
-  
-  g0 <- ggplot(data=melt(df, "id"), aes(as.factor(variable), value, colour=id, group=id))
-  
-  my.fun<-function(x){data.frame(ymin=min(x),ymax=max(x),y=mean(x))}
-  g_box = g0 + 
-    stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size=0.5, alpha=0.5) + 
-    stat_summary(fun.y = mean, geom = "line", aes(group=id, linetype=id), size=1.1) + 
-    scale_linetype_manual(values=c("solid", "dashed", "dotted")) +
-    theme_bw() + theme(panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank(),
-                       panel.grid.minor.x = element_blank(), panel.grid.major.y = element_blank()) +
-    ylim(0,100) +
-    xlab("Batch") + ylab("% class. error") + ggtitle(dataset)
-  
-  g_box
-  ggsave(file=paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\plots-adaptive\\", dataset, ".png", sep=""))
-
 }
 
-# OLD PLOTS
-#   png(paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\plots-adaptive\\", dataset, ".png", sep=""))
-#   xrange = c(0,9)
-#   yrange = c(0,100)
-#   plot(xrange, yrange, type="n", xlab="Batch", ylab="Test error")
-#   axis(1, at=seq(0,9))
-#   title(dataset)
-#   #colors = rainbow(3)
-#   # colorblind-friendly palette
-#   colors = c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-#   ltype = c(1,2,3)
-#   symbols = c(0,1,2)
-#   
-#   lines(0:9, colMeans(test_error_adaptive, na.rm=TRUE), type="b", col=colors[1], lty=ltype[1], pch=symbols[1], lwd=2)
-#   lines(0:9, colMins(test_error_adaptive, na.rm=TRUE), type="b", col=adjustcolor(colors[1],alpha.f=0.8), lty=ltype[1], pch=symbols[1], lwd=1)
-#   
-#   lines(0:9, colMeans(test_error_retrain, na.rm=TRUE), type="b", col=colors[2], lty=ltype[2], pch=symbols[2], lwd=2)
-#   lines(0:9, colMins(test_error_retrain, na.rm=TRUE), type="b", col=adjustcolor(colors[2],alpha.f=0.8), lty=ltype[2], pch=symbols[2], lwd=1)
-#   
-#   lines(0:9, colMeans(test_error_static, na.rm=TRUE), type="b", col=colors[3], lty=ltype[3], pch=symbols[3], lwd=2)
-#   lines(0:9, colMins(test_error_static, na.rm=TRUE), type="b", col=adjustcolor(colors[3],alpha.f=0.8), lty=ltype[3], pch=symbols[3], lwd=1)
-#   
-#   legend('topleft','groups', c("Adapt","Retrain", "Static"), lty=ltype, col=colors, pch=symbols, ncol=3, bty ="n", lwd=2)
-#   dev.off()
 
+###########################################
+# FROM FILES -- REGRESSION
+###########################################
+
+datasets = c("absorber", "catalyst", "debutanizer", "IndustrialDrier", "oxeno", "sulfur", "ThermalOxidizerConv")
+
+run_regression = function(datasets, metric_name) {
+  
+  metric = match.fun(metric_name)
+  
+  for (d in 1:length(datasets)) {
+    
+    print(datasets[d])
+    dataset = datasets[d]
+    
+    all_instances = matrix(0, 5, 25)
+    all_test_error = matrix(0, 5, 25)
+    
+    test_error_adaptive = matrix(0, 25, 10)
+    for (i in 0:24) {
+      for (j in 0:9) {
+        filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\adaptive-regression\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
+        #print(paste("Reading", filename))
+        if (file.exists(filename)) {
+          predictions = read.csv(filename)
+          batch = as.data.frame(predictions)
+          batch$error = as.numeric(batch$error)
+          test_error_adaptive[i+1, j+1] = metric(as.numeric(batch$actual), as.numeric(batch$predicted))
+          #all_instances[1,i+1] = all_instances[1,i+1] + nrow(batch)
+          #all_test_error[1,i+1] = all_test_error[1,i+1] + sum(abs(as.numeric(batch$actual) - as.numeric(batch$predicted)))
+        }
+        else {
+          print(paste("File", filename, "does not exists"))
+          test_error_adaptive[i+1,j+1] = NaN
+        }
+      }
+    }
+    
+    test_error_retrain = matrix(0, 25, 10)
+    for (i in 0:24) {
+      for (j in 0:9) {
+        filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\static-regression\\", dataset,"\\batch",j,"\\predictions.", i, ".csv", sep="")
+        #print(paste("Reading", filename))
+        if (file.exists(filename)) {
+          predictions = read.csv(filename)
+          batch = as.data.frame(predictions)
+          test_error_retrain[i+1, j+1] = metric(as.numeric(batch$actual), as.numeric(batch$predicted))
+          #print(paste(i+1, j+1, test_error_retrain[i+1, j+1], batch$actual, sep=" "))
+        }
+        else {
+          print(paste("File", filename, "does not exists"))
+          test_error_retrain[i+1,j+1] = NaN
+        }
+      }
+    }
+    
+    test_error_static = matrix(0, 25, 10)
+    for (i in 0:24) {
+      filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\non-adaptive-regression\\", dataset,"\\predictions.", i, ".csv", sep="")
+      if (file.exists(filename)) {
+        predictions = read.csv(filename)
+        batches  = split(predictions, ceiling(seq_along(predictions$inst.)/(nrow(predictions)/10)))
+        for (j in 1:10) {
+          batch = as.data.frame(batches[j])
+          names(batch) = c("inst", "actual", "predicted", "error")
+          test_error_static[i+1, j] = metric(as.numeric(batch$actual), as.numeric(batch$predicted))
+          #print(paste(i, j, test_error_static[i+1, j], batch[2], batch[3], sep=" "))
+        }
+      }
+      else {
+        print(paste("File", filename, "does not exists"))
+        test_error_static[i+1,] = NaN
+      }
+    }
+    
+    
+    m1 = as.data.frame(test_error_adaptive)
+    m1$id = "Batch + SMAC"
+    m2 = as.data.frame(test_error_retrain)
+    m2$id = "Batch"
+    m3 = as.data.frame(test_error_static)
+    m3$id = "Baseline"
+    df = rbind(m1, m2, m3)
+    
+    
+    g0 <- ggplot(data=melt(df, "id"), aes(as.factor(variable), value, colour=id, group=id))
+    
+    my.fun<-function(x){data.frame(ymin=min(x),ymax=max(x),y=mean(x))}
+    g_box = g0 + 
+      stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width=0.2, size=0.5, alpha=0.5) + 
+      stat_summary(fun.y = mean, geom = "line", aes(group=id, linetype=id), size=1.1) + 
+      scale_linetype_manual(values=c("solid", "dashed", "dotted")) +
+      theme_bw() + theme(panel.grid.major.x = element_line(colour = "gray"), panel.grid.major.y = element_blank(),
+                         panel.grid.minor.x = element_blank(), panel.grid.major.y = element_blank()) +
+      xlab("Batch") + ylab(metric_name) + ggtitle(dataset)
+    
+    g_box
+    ggsave(file=paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\plots-adaptive-regression\\", dataset, ".", metric_name, ".png", sep=""))
+    
+    names_error = c("Batch + SMAC", "Batch", "Baseline")
+    all_text = ""
+    per_seed_text = ""
+    per_seed_error = data.frame(NA, 5, 25)
+    for(i in 1:5) {
+      current_error = matrix(NA, 25, 1)
+      for (j in 1:25) {
+        current_error[j] = all_test_error[i,j]/all_instances[i,j]
+        per_seed_error[i, j] = current_error[j]
+        
+      }
+      all_text = sprintf("%s\n%s, %f", all_text, names_error[i], mean(current_error, na.rm=TRUE))
+      per_seed_text = sprintf("%s\n%s, %f", per_seed_text, names_error[i], min(per_seed_error[i,], na.rm=TRUE))
+    }
+    write.csv(paste(all_text, per_seed_text, sep="\n"), file=paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\plots-incremental\\", dataset, ".csv", sep=""))
+  }
+}
+
+datasets = c("absorber", "catalyst", "debutanizer", "IndustrialDrier", "oxeno", "sulfur", "ThermalOxidizerConv")
+
+plot_target_value = function(datasets) {
+  for (d in 1:length(datasets)) {
+    
+    print(datasets[d])
+    dataset = datasets[d]
+    
+    actual = c()
+    predicted = c()
+    
+    for (batch in 0:9) {
+      seed = 0
+      filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\adaptive-regression\\",dataset,"\\batch",batch,"\\predictions.",seed,".csv", sep="")
+      while (!file.exists(filename) && seed<25) {
+        seed = seed+1
+        filename = paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\experiments\\adaptive-regression\\",dataset,"\\batch",batch,"\\predictions.",seed,".csv", sep="")
+      }
+      data = as.data.frame(read.csv(filename))
+      actual = append(actual, data$actual)
+      predicted = append(predicted, data$predicted)
+    }
+    
+    data = data.frame(1:length(actual), actual, predicted)
+    names(data) = c("time", "actual", "predicted")
+    
+    #ggplot(data, aes(time)) + 
+    #  geom_line(aes(y = actual, colour = "actual")) + 
+    #  geom_line(aes(y = predicted, colour = "predicted"))
+    
+    
+    g_box = ggplot(data, aes(time)) + 
+      geom_line(aes(y = actual, colour = "actual")) +
+      geom_vline(xintercept = seq(0, nrow(data), nrow(data)/10), colour="green", linetype = "longdash") + 
+      theme_bw() + theme(panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank(),
+                         panel.grid.minor.x = element_blank(), panel.grid.major.y = element_blank())
+    
+    g_box
+    ggsave(file=paste("C:\\Users\\Manuel\\workspace-autoweka\\autoweka\\plots-adaptive-regression\\", dataset, ".actual.batches.png", sep=""))
+  }
+}
